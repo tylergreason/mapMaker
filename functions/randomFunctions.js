d162b736645726ce45dc0dc4de687c952f1b0884
tylergreason <tyler.greason@protonmail.com>
2020-08-01 17:18:10 -0400

    made random dice sides times number of dice function

--- functions/randomFunctions.js
+++ functions/randomFunctions.js
@@ -1,7 +1,8 @@
-const randomWhole = (sides, quantity=1) => {
+const dx = (sides, quantity=1) => {
     let total = 0; 
-    for (let i = 0; i <= quantity; i++){
+    for (let i = 0; i < quantity; i++){
         total += Math.floor(Math.random() * sides) +1; 
     }
+    console.log(total)
     return total;
 }
