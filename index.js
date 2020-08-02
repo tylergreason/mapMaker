e660c7e83f50035a162870fc03dd5e7104c108c4
tylergreason <tyler.greason@protonmail.com>
2020-08-02 13:36:09 -0400

    made function to console log map

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 console.log('hello, world')
 
 // create function to create x by y room 
-const createRoom = size => {
+const createRoom = (width, height=width, glyph='0') => {
     // let width = dx(size)+10; 
     // let height = dx(size)+10; 
 
-    let width = 10; 
-    let height = 15; 
+    // let width = 10; 
+    // let height = 15; 
     // create array 
     let room = [];
     for (let i = 0; i < width; i++){
         room.push([])
         for (let j = 0; j < height; j++){
-            room[i].push([])
+            room[i].push([glyph])
         }
     }
     return room; 
 }
 
-let myRoom = createRoom(10); 
-console.log(myRoom); 
+// use static sized level for now 
+const level = createRoom(5); 
+console.log(level)
+
+const displayRoom = array => {
+    let returnString = ''; 
+    // loop through length of array and print each line 
+    array.forEach(line => {
+        let string = ''; 
+        line.forEach(ele => string += ele);
+        returnString += string + "\n"
+    })
+    console.log(returnString)
+}
+
+level[3][0] = "d"
+displayRoom(level)
