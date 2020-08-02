160c411537f249a637a92c8479f057cf13cd3d89
tylergreason <tyler.greason@protonmail.com>
2020-08-02 13:26:17 -0400

    Made level generation algo

--- index.js
+++ index.js
@@ -2,5 +2,21 @@ console.log('hello, world')
 
 // create function to create x by y room 
 const createRoom = size => {
-    
+    // let width = dx(size)+10; 
+    // let height = dx(size)+10; 
+
+    let width = 10; 
+    let height = 15; 
+    // create array 
+    let room = [];
+    for (let i = 0; i < width; i++){
+        room.push([])
+        for (let j = 0; j < height; j++){
+            room[i].push([])
+        }
+    }
+    return room; 
 }
+
+let myRoom = createRoom(10); 
+console.log(myRoom); 
