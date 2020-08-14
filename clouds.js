a6470bfc9650679f6ef703a200c7b3c2eac6f904
tylergreason <tyler.greason@protonmail.com>
2020-08-14 09:41:41 -0400

    Generated some tiny clouds

--- clouds.js
+++ clouds.js
@@ -2,13 +2,39 @@
 const cloudSpeed = 10;  
 const cloudSize = 5; 
 
+const mapHeight = document.getElementById('map').clientHeight; 
+const mapWidth = document.getElementById('map').clientWidth;
 const generateCloud = () => {
     let ele = document.createElement('div'); 
     applyStyles(cloud, ele); 
-    ele.style.width = cloudSize + 'px'; 
-    ele.style.height = cloudSize + 'px'; 
-    ele.style.position = "absolute"; 
+    cloudStyles(ele); 
+    map.appendChild(ele); 
+    cloudAnimation(ele); 
+}
+
+const cloudStyles = cloud => {
+    cloud.style.width = cloudSize + 'px'; 
+    cloud.style.height = cloudSize + 'px'; 
+    cloud.style.position = "absolute"; 
     // set height & starting width
-    ele.style.top = Math.floor(Math.random()*mapHeight);
-    ele.style.left = Math.floor(Math.random()*mapWidth);
+    cloud.style.top = Math.floor(Math.random()*mapHeight);
+    cloud.style.left = Math.floor(Math.random()*mapWidth);
+}
+
+const cloudKeyframes = [
+    {
+        left: '0px'
+    },
+    {
+        left: `100px`
+    }
+]
+
+const cloudAnimationOptions = {
+    duration: 1000, 
+    fill: 'forwards'
+}
+
+const cloudAnimation = cloud => {
+    cloud.animate(cloudKeyframes, cloudAnimationOptions)
 }
